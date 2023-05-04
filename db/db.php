eafe70e7f82351fd49ea11cfc2f264fbd9d015a8
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-04 16:06:43 +0200

    organizzato il codice in cartelle

---
+++ db/db.php
@@ -0,0 +1,23 @@
+<?php
+
+$allGenres = [ "Fantasy", "Action", "Adventure"];
+
+$dragon_heart = new Movie("Dragon Heart", $allGenres, "Rob Cohen");
+
+$il_signore_degli_anelli = new Movie("Il signore degli anelli", $allGenres,"Peter Jackson");
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,27 +1,34 @@
 <?php
 
-/* definisci una classe ‘Movie’
-all'interno della classe sono dichiarate delle variabili d'istanza
-all'interno della classe è definito un costruttore
-all'interno della classe è definito almeno un metodo
-vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà */
-
-require __DIR__ . '/Models/Movie.php';
-
-$allGenres = [ "Fantasy", "Action", "Adventure"];
-
-$dragon_heart = new Movie("Dragon Heart", $allGenres, "Rob Cohen");
-echo  '<h1>Titolo:</h1>'. $dragon_heart->get_title() . '<br>';
-echo '<h2>Genere:</h2>'. $dragon_heart->get_All_genres() . '<br>';
-echo '<h3>Diretto da:</h3>'. $dragon_heart->get_director() . '<br>';
-
-
-$il_signore_degli_anelli = new Movie("Il signore degli anelli", $allGenres,"Peter Jackson");
-echo '<h1>Titolo:</h1>'.  $il_signore_degli_anelli->get_title() . '<br>';
-echo '<h2>Genere:</h2>'. $il_signore_degli_anelli->get_All_genres() . '<br>';
-echo  '<h3>Diretto da:</h3>'. $il_signore_degli_anelli->get_director() ;
-
-
+require_once __DIR__."\Models\Movie.php";
+require_once __DIR__."\db\db.php";
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+
+
+<body>
+  <?php
+  
+  echo  '<h1>Titolo:</h1>'. $dragon_heart->get_title() . '<br>';
+  echo '<h2>Genere:</h2>'. $dragon_heart->get_All_genres() . '<br>';
+  echo '<h3>Diretto da:</h3>'. $dragon_heart->get_director() . '<br>';
+
+  echo '<h1>Titolo:</h1>'.  $il_signore_degli_anelli->get_title() . '<br>';
+    echo '<h2>Genere:</h2>'. $il_signore_degli_anelli->get_All_genres() . '<br>';
+    echo  '<h3>Diretto da:</h3>'. $il_signore_degli_anelli->get_director() ;
+
+  ?>
+
+    
+</body>
+</html>
